6a6b45fff248fcb87014800dd65a30c80453738d
Your Name <you@example.com>
2023-04-25 16:44:46 -0700

    create new widget named custom_note_item

--- lib/view/widgets/notes_view_body.dart
+++ lib/view/widgets/notes_view_body.dart
@@ -21,40 +21,4 @@ class NotesViewbody extends StatelessWidget {
   }
 }
 
-class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top:24,bottom: 24),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        
-        border: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text("fluter tips",style: TextStyle(color: Colors.black
-            
-            ),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text("builled"),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icon.delete,color: Colors.black,),
-            ),
-          )
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text('may21, 2022',style: TextStyle(color: Colors.black),),
-        )
-        ],
-      ),
-    );
-  }
-}

27b5ee93f98e081e514dcc7250cac20105a41cf2
Your Name <you@example.com>
2023-04-25 05:04:18 -0700

    create new view inside the LIB

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark
-      scaffoldBackgroundColor: ),
+      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(),
     );
   }

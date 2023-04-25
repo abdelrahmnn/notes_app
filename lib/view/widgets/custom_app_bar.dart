87483432a3ad3cca513cda44be7071a3c69815a0
Your Name <you@example.com>
2023-04-25 08:19:33 -0700

    create widget named custom_icon

--- lib/view/widgets/custom_app_bar.dart
+++ lib/view/widgets/custom_app_bar.dart
@@ -17,24 +17,3 @@ class CustomAppbar extends StatelessWidget {
     );
   }
 }
-
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 45,
-      decoration: BoxDecoration(
-          color: Colors.white.withOpacity(.5),
-          borderRadius: BorderRadius.circular(16)),
-      child: Center(
-        child: Icon(
-          Icon.search,
-          size: 28,
-        ),
-      ),
-    );
-  }
-}

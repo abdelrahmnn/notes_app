4178fb7996e20ed7d30a07887e8dad263f31584a
Your Name <you@example.com>
2023-04-25 05:43:20 -0700

    do import for widget here

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:notes_app/view/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});

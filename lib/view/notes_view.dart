0f42c08ed2a6fab12d5baa51dacdd51619d9f79a
Your Name <you@example.com>
2023-04-25 05:24:13 -0700

    create new widget

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -12,3 +12,12 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+
+class NotesViewbody extends StatelessWidget {
+  const NotesViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

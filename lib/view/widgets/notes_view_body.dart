b2b2f4556df807a8e7f0d74cd391c10e9e4eb2ef
Your Name <you@example.com>
2023-04-25 05:39:23 -0700

    call the import metaril.dart

---
+++ lib/view/widgets/notes_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesViewbody extends StatelessWidget {
+  const NotesViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
